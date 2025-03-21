c217cc6998795ae8dba55404be7c24ae045001f9
Soe-Yujeong <yseo3760@gmail.com>
2025-03-21 10:27:33 +0900

    feat: moveCar 메소드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,20 +1,33 @@
 package racingcar;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Car {
-    String carName;
-    int position = 0;
 
-    Scanner sc = new Scanner(System.in);
-
-    public Car(String carName, int position){
-        this.carName = carName;
-        this.position = position;
+    public String getCar(String carName){
+        return carName;
     }
 
-    public void writeName(String carName){
-        carName = sc.nextLine();
-        String[] carArr = carName.split(",");
+    public void writeName(){ //차 이름 받기
+        String cars = readLine();
+        String[] carArr = cars.split(",");
+        List<Car> carList = new ArrayList<>();
+
+        for (int i =0; i < carList.size(); i++){ //리스트에 차 이름 넣기
+            carList.get(i);
+        }
     }
+
+    public void moveCar(int position){ //차 움직이기
+        int rand = pickNumberInRange(1, 9);
+
+        if(rand >= 4){
+            position +=1;
+        }
+    }
+
+
 }
